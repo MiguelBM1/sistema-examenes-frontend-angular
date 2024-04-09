d21d1dd51245a888d8fcde59f54e4c2fb68922c6
MiguelBM1 <bm15056@ues.edu.sv>
2024-04-08 21:05:10 -0600

    agregando navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   standalone: true,
   styleUrls: ['./navbar.component.css'],
-  imports: [MatToolbarModule, MatIconModule]
+  imports: [MatToolbarModule, MatIconModule, RouterLink]
 })
 export class NavbarComponent implements OnInit {
 
